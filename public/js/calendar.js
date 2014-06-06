6645f4d5ca7dca7467b1623ef205985819a164f5
Emily Namugaanyi <emilynam@thoughtworks.com>
2014-06-06 14:14:19 +0300

    Add calendar with weekly events

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -4,6 +4,12 @@ $(document).ready(function() {
 
     $('#calendar').fullCalendar({
         // put your options and callbacks here
+        columnFormat: {
+          month: 'ddd', 
+          week: 'ddd dd/MM',
+          day: 'dddd M/d'
+        },
+        events: 'https://www.google.com/calendar/feeds/churchchriscobugos%40gmail.com/public/basic',
+            
     })
-
 });
